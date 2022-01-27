3c386276f9967dc293c01632b8feb39f99e815b4
felmez <sofelmez@gmail.com>
2022-01-27 11:43:09 +0300

    nodemailer_setup

---
+++ src/utils/mailer.js
@@ -0,0 +1,35 @@
+const nodemailer = require('nodemailer');
+const logger = require('./logger');
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+const sendEmail = (user) => {
+  const mailOptions = {
+    from: process.env.EMAIL,
+    to: user.email,
+    subject: 'Welcome | Your account created successfully',
+    text: `You can login the to platform by the information below: \n
+    Login URL: http://localhost:3000
+    Email: ${user.email} \n
+    Password: The password used while registration \n
+    Thank you for using Bursapediary.`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      logger.info(error);
+      return error;
+    }
+    logger.info(info.response);
+    return info.response;
+  });
+};
+
+module.exports = sendEmail;
