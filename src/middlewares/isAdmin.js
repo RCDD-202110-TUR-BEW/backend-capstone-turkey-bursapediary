f5fbd56e956e3d088316064b62afd19b9fcf9638
felmez <sofelmez@gmail.com>
2022-01-31 17:57:35 +0300

    edit logic in isAdmin middleware

--- src/middlewares/isAdmin.js
+++ src/middlewares/isAdmin.js
@@ -5,28 +5,15 @@ const { SECRET_KEY } = process.env;
 const isAdmin = (req, res, next) => {
   const { token } = req.cookies;
 
-  if (token) {
-    try {
-      const loggedUser = jwt.verify(token, SECRET_KEY);
-      if (loggedUser.role === 'admin') {
-        req.user = loggedUser;
-        next();
-      } else {
-        return res.clearCookie('token').status(403).json({
-          error: 'You do not have permissions to perform this action',
-        });
-      }
-    } catch (error) {
-      return res
-        .clearCookie('token')
-        .status(422)
-        .json({ error: 'Token expired try to login' });
-    }
+  const loggedUser = jwt.verify(token, SECRET_KEY);
+
+  if (loggedUser.role === 'admin') {
+    next();
   }
-  return res
-    .clearCookie('token')
-    .status(401)
-    .json({ error: 'Token is not found try to login' });
+
+  return res.clearCookie('token').status(403).json({
+    error: 'You do not have permissions to perform this action',
+  });
 };
 
 module.exports = {
