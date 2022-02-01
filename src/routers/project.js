317a0a381841c58400706b4b07de4025d8a2df87
amr-nash <amr.nash@gmail.com>
2022-02-01 23:11:05 +0300

    fix some syntax

--- src/routers/project.js
+++ src/routers/project.js
@@ -1,14 +1,14 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
-const project = require('../controllers/project')
+const project = require('../controllers/project');
 
-router.get('/', project.getAllProjects)
-router.get('/:id', project.getProjectByID)
-router.get('/:id/comments/', project.getAllComments)
-router.get('/:id/comments/:commentid', project.getCommentByID)
-router.get('/:id/reviews/', project.getAllReviews)
-router.get('/:id/reviews/:remiewid', project.getReviewByID)
+router.get('/', project.getAllProjects);
+router.get('/:id', project.getProjectByID);
+router.get('/:id/comments/', project.getAllComments);
+router.get('/:id/comments/:commentid', project.getCommentByID);
+router.get('/:id/reviews/', project.getAllReviews);
+router.get('/:id/reviews/:reviewid', project.getReviewByID);
 
 module.exports = router;
