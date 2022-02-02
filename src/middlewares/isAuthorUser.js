74ee2f5e6b513240635b0bd600949148ade0a83e
felmez <sofelmez@gmail.com>
2022-02-02 03:43:42 +0300

    Feat: add isAuthorUser middleware, close #14

---
+++ src/middlewares/isAuthorUser.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-underscore-dangle */
+const isAuthorUser = (object, user, type) => {
+  switch (type) {
+    case 'user':
+      if (object._id === user._id) return true;
+      break;
+    case 'project':
+      if (object.owners.includes(user._id)) return true;
+      break;
+    case 'comment':
+      if (
+        object.comments[
+          object.comments.findIndex((comment) => comment._id === user._id)
+        ] !== -1
+      )
+        return true;
+      break;
+    case 'review':
+      if (
+        object.reviews[
+          object.reviews.findIndex((review) => review._id === user._id)
+        ] !== -1
+      )
+        return true;
+      break;
+    default:
+      return false;
+  }
+  return false;
+};
+
+module.exports = isAuthorUser;
