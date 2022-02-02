204c5b4be5430a3f31150cc392cc6f3f56d5dd57
yaman <yaman3bd@gmail.com>
2022-02-02 12:54:59 +0300

    filter_projects

---
+++ src/controllers/project.js
@@ -0,0 +1,21 @@
+const Project = require('../models/project');
+
+const filterProjects = async (req, res) => {
+  try {
+    const { q } = req.query;
+    // the user do not necessarily search for a project by its category
+    // try to search in the category is not found try other things
+    // do not ever show the user empty or a message says we are sorry we do not have what you are asking :)
+    const projects = await Project.find({
+      $or: [
+        { category: { $elemMatch: { $regex: q } } },
+        { title: { $elemMatch: { $regex: q } } },
+        { description: { $elemMatch: { $regex: q } } },
+      ],
+    });
+    res.status(200).json(projects);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+};
+module.exports = { filterProjects };
