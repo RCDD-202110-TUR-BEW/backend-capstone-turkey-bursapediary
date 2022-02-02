42a1862b60cec2938752cd1fee5b4ce7e5178722
felmez <sofelmez@gmail.com>
2022-02-02 03:48:29 +0300

    Fix: point to object user #14

--- src/middlewares/isAuthorUser.js
+++ src/middlewares/isAuthorUser.js
@@ -10,7 +10,7 @@ const isAuthorUser = (object, user, type) => {
     case 'comment':
       if (
         object.comments[
-          object.comments.findIndex((comment) => comment._id === user._id)
+          object.comments.findIndex((comment) => comment.user._id === user._id)
         ] !== -1
       )
         return true;
@@ -18,7 +18,7 @@ const isAuthorUser = (object, user, type) => {
     case 'review':
       if (
         object.reviews[
-          object.reviews.findIndex((review) => review._id === user._id)
+          object.reviews.findIndex((review) => review.user._id === user._id)
         ] !== -1
       )
         return true;
