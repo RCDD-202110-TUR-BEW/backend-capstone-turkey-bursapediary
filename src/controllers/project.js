46b9e7f73f4d2e81a8418be5b3b11597d5828385
felmez <sofelmez@gmail.com>
2022-02-05 15:35:19 +0300

    Feat: update review on project ep, closes #25

--- src/controllers/project.js
+++ src/controllers/project.js
@@ -1,3 +1,5 @@
+const { ObjectId } = require('mongodb');
+
 const Project = require('../models/project');
 
 const createReview = async (req, res, next) => {
@@ -24,6 +26,35 @@ const createReview = async (req, res, next) => {
   return next();
 };
 
+const isEmpty = (field) => !!field;
+
+const updateReview = async (req, res, next) => {
+  const { id, reviewId } = req.params;
+  const { rating, content } = req.body;
+
+  try {
+    const project = await Project.findById(id);
+
+    if (!project) return res.status(404).json({ message: 'Project not found' });
+    const review = project.reviews.find(
+      // eslint-disable-next-line no-underscore-dangle
+      (single) => ObjectId(single._id).toString() === reviewId
+    );
+
+    if (!review) return res.status(404).json({ message: 'Review not found' });
+
+    review.rating = isEmpty(rating) ? rating : review.rating;
+    review.content = isEmpty(content) ? content : review.content;
+
+    await project.save();
+    res.json(project.reviews);
+  } catch (error) {
+    res.status(422).json({ message: 'Unable to update review' });
+  }
+  return next();
+};
+
 module.exports = {
   createReview,
+  updateReview,
 };
--- src/routers/project.js
+++ src/routers/project.js
@@ -5,7 +5,7 @@ const project = require('../controllers/project');
 const routes = express.Router();
 
 routes.post('/:id/reviews', project.createReview);
-// routes.put('/:id/reviews/:reviewId', project.updateReview);
+routes.put('/:id/reviews/:reviewId', project.updateReview);
 // routes.delete('/:id/reviews/:reviewId', project.deleteReview);
 
 module.exports = routes;
