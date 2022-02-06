10270af7f989851d1e9aa512f9eaf3a211422d08
Coruh Durmus <coruh.d@gmail.com>
2022-02-06 03:41:16 +0300

    add homepage

--- src/routers/index.js
+++ src/routers/index.js
@@ -5,7 +5,13 @@ const router = express.Router();
 const UserRoutes = require('./user');
 const ProjectRoutes = require('./project');
 
+const path = require('path')
+
 router.use('/users', UserRoutes);
 router.use('/projects', ProjectRoutes);
+router.get('/',function(req,res){
+    res.render(path.join(__dirname+'/../views/index.ejs'));
+  });
+
 
 module.exports = router;
