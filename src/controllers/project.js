1b1e478bca66a29ce38e50e0c2424c272416c8f7
yaman <yaman3bd@gmail.com>
2022-02-08 11:44:25 +0300

    add filter and search functions to projects

--- src/controllers/project.js
+++ src/controllers/project.js
@@ -2,15 +2,27 @@ const Project = require('../models/project');
 
 const filterProjects = async (req, res) => {
   try {
-    const { q } = req.query;
+    const { category } = req.query;
+
+    const projects = await Project.find({ 'category.content': category });
+
+    return res.json(projects);
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+};
+
+const searchProjects = async (req, res) => {
+  try {
+    const { search } = req.query;
     // the user do not necessarily search for a project by its category
     // try to search in the category is not found try other things
     // do not ever show the user empty or a message says we are sorry we do not have what you are asking :)
     const projects = await Project.find({
       $or: [
-        { category: { $elemMatch: { $regex: q } } },
-        { title: { $elemMatch: { $regex: q } } },
-        { description: { $elemMatch: { $regex: q } } },
+        { category: { $elemMatch: { $regex: search } } },
+        { title: { $elemMatch: { $regex: search } } },
+        { description: { $elemMatch: { $regex: search } } },
       ],
     });
     return res.json(projects);
@@ -18,4 +30,5 @@ const filterProjects = async (req, res) => {
     return res.status(400).json(error);
   }
 };
-module.exports = { filterProjects };
+
+module.exports = { filterProjects, searchProjects };
