af45273052dd7b0dbff0e3a9182ff2d8d5590b54
felmez <sofelmez@gmail.com>
2022-02-08 15:24:54 +0300

    Refactor: fetch callback url from env, closes #30

--- src/auth/passport.js
+++ src/auth/passport.js
@@ -9,7 +9,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3000/auth/google/callback',
+      callbackURL: process.env.GOOGLE_CALL_BACK_URL,
     },
     async (accessToken, refreshToken, profile, callBack) => {
       const user = await User.findOne({ providerId: profile.id });
