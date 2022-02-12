0256f8133e0b26ae1bb069efbd31591eb8a44a39
felmez <sofelmez@gmail.com>
2022-02-12 17:18:43 +0300

    Refactor: add some test setup, closes #44

--- src/utils/__tests__/mailer.test.js
+++ src/utils/__tests__/mailer.test.js
@@ -3,11 +3,17 @@ const chaiHttp = require('chai-http');
 
 const app = require('../../app');
 
+const chaiAppServer = chai.request(app).keepOpen();
+
 chai.should();
 
 chai.use(chaiHttp);
 
 describe('Some description', () => {
+  after(() => {
+    chaiAppServer.close();
+  });
+
   it('Some task', (done) => {
     done();
   });
