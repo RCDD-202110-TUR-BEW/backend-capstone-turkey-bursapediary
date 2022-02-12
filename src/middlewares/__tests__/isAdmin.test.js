aa3b333151516f27f1ed74e494840dc719243d0c
felmez <sofelmez@gmail.com>
2022-02-12 17:41:15 +0300

    Refactor: add some test setup, closes #44

--- src/middlewares/__tests__/isAdmin.test.js
+++ src/middlewares/__tests__/isAdmin.test.js
@@ -1,19 +1,3 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-
-const app = require('../../app');
-
-chai.should();
-chai.use(chaiHttp);
-
-const chaiAppServer = chai.request(app).keepOpen();
-
 describe('Some description', () => {
-  after(() => {
-    chaiAppServer.close();
-  });
-
-  it('Some task', (done) => {
-    done();
-  });
+  it('Some task', () => {});
 });
