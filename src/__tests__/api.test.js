c58243874efa8ecad82622f463ef7382736747cc
yaman <yaman3bd@gmail.com>
2022-02-17 18:54:18 +0300

    add project filter test and fix some conflict errors

--- src/__tests__/api.test.js
+++ src/__tests__/api.test.js
@@ -58,6 +58,26 @@ const newReview = {
 };
 
 describe('Project Routes', () => {
+  describe('GET Filter', () => {
+    it('Should retrun empty array if no projects matching category', async () => {
+      const res = await request(app)
+        .get('/projects/filter?category=undefinedtestnotrealycategorynotfund')
+        .set('Content-Type', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200);
+      expect(Array.isArray(res.body)).toBe(true);
+      expect(res.body).toHaveLength(0);
+    });
+    it('Should retrun projects matching category', async () => {
+      const res = await request(app)
+        .get('/projects/filter?category=testing')
+        .set('Content-Type', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200);
+      expect(Array.isArray(res.body)).toBe(true);
+      expect(res.body).toHaveLength(1);
+    });
+  });
   describe('GET /projects', () => {
     it('Should return all projects from db', async () => {
       const res = await request(app).get(`/projects`);
