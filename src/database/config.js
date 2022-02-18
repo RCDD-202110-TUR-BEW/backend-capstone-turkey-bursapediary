296b30fd493c9ed2e2748e06b8eaa677b51fdb4a
felmez <sofelmez@gmail.com>
2022-02-18 04:20:14 +0300

    Fix: resolve ci conflicts and singleton db

--- src/database/config.js
+++ src/database/config.js
@@ -1,26 +1,38 @@
+/* eslint-disable max-classes-per-file */
 const mongoose = require('mongoose');
 const logger = require('../utils/logger');
 
-let URI;
+class PrivateSingleton {
+  constructor() {
+    const db = mongoose.connection;
 
-if (process.env.NODE_ENV === 'test') {
-  URI = process.env.TEST_DB_URI;
-} else {
-  URI = process.env.DB_URI;
+    if (process.env.NODE_ENV === 'test') {
+      this.URI = process.env.TEST_DB_URI;
+    } else {
+      this.URI = process.env.DB_URI;
+    }
+    mongoose.connect(this.URI, { useNewUrlParser: true });
+
+    db.once('open', () => {
+      logger.info(`Database connected: ${this.URI}`);
+    });
+
+    db.on('error', (err) => {
+      logger.info(`Database connection error: ${err}`);
+    });
+  }
+}
+class SingletonMongoDB {
+  constructor() {
+    throw new Error('Use Singleton.getInstance()');
+  }
+
+  static getInstance() {
+    if (!SingletonMongoDB.instance) {
+      SingletonMongoDB.instance = new PrivateSingleton();
+    }
+    return SingletonMongoDB.instance;
+  }
 }
 
-const DBConnection = () => {
-  mongoose.connect(URI, { useNewUrlParser: true });
-
-  const db = mongoose.connection;
-
-  db.once('open', () => {
-    logger.info(`Database connected: ${URI}`);
-  });
-
-  db.on('error', (err) => {
-    logger.info(`Database connection error: ${err}`);
-  });
-};
-
-module.exports = DBConnection;
+module.exports = SingletonMongoDB;
